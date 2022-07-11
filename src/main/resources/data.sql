1d834ed3fe061fd0ef808b1db036423abc74b7c6
massanhm <changshangq@gmail.com>
2022-07-12 01:50:13 +0900

    add データベース設定とテストデータを追加

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+--password1234
+--INSERT INTO USERS (username, password) VALUES ('test1', 'password1234');
+--INSERT INTO USERS (username, password) VALUES ('test2', 'password1234');
+
+--INSERT INTO inventorys (report_date, product, quantity) VALUES ('2022-04-01', 'test', '1.1');
+
+INSERT INTO products (product_id, name) VALUES ('1', 'test1');
+INSERT INTO products (product_id, name) VALUES ('2', 'test2');
