4d7650ef7620d4930cc5e9ffb7c146a7074bb69a
massanhm <changshangq@gmail.com>
2022-07-19 10:44:46 +0900

    Add 在庫情報のテストデータを追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,8 @@
 --INSERT INTO USERS (username, password) VALUES ('test1', 'password1234');
 --INSERT INTO USERS (username, password) VALUES ('test2', 'password1234');
 
---INSERT INTO inventorys (report_date, product, quantity) VALUES ('2022-04-01', 'test', '1.1');
+INSERT INTO inventorys (products_id, quantity, report_date) VALUES ('1', '3.0', '2022-07-07 15:00:00');
+INSERT INTO inventorys (products_id, quantity, report_date) VALUES ('2', '4.5', '2022-07-08 15:00:00');
 
 INSERT INTO products (name) VALUES ('test1');
 INSERT INTO products (name) VALUES ('test2');
