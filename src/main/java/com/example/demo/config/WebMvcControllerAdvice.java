f25268e89953d26023b424be4068e724a43bc2a3
massanhm <changshangq@gmail.com>
2022-07-19 09:30:37 +0900

    Add 商品名に関する例外処理を追加

--- src/main/java/com/example/demo/config/WebMvcControllerAdvice.java
+++ src/main/java/com/example/demo/config/WebMvcControllerAdvice.java
@@ -1,9 +1,14 @@
 package com.example.demo.config;
 
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
+
+import com.example.demo.Service.ProductNotFoundException;
+
 
 @ControllerAdvice
 public class WebMvcControllerAdvice {
@@ -16,4 +21,9 @@ public class WebMvcControllerAdvice {
         dataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
     }
 
+    @ExceptionHandler(ProductNotFoundException.class)
+    public String handleException(ProductNotFoundException e,Model model) {
+        model.addAttribute("message", e);
+        return "error/CustomPage";
+    }
 }
