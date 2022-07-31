752930b57da2d1db71a0141b93b0792739896e42
massanhm <changshangq@gmail.com>
2022-07-31 23:32:25 +0900

    Remove データ項目を一部削除
    最終の更新記録さえ残っていれば良いので
    作成日時は不要と判断し、削除した。

--- src/main/java/com/example/demo/repository/InventoryMapper.java
+++ src/main/java/com/example/demo/repository/InventoryMapper.java
@@ -12,10 +12,8 @@ import com.example.demo.entity.Inventory;
 @Mapper
 public interface InventoryMapper {
 
-//    @Select("SELECT inventorys.id, products_id, quantity, report_date, created_at, updated_at, name "
-//           + "FROM inventorys "
-//            + "INNER JOIN products ON inventorys.products_id = products.id "
-//            + "WHERE report_date = #{sqlDate}")
     List<Inventory> findByDate(Date sqlDate);
+
+    void insert(Inventory inventory);
 
 }
