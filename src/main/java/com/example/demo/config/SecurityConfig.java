44c56f2e85708b81324f085b85b4823e5e358e54
massanhm <changshangq@gmail.com>
2022-09-23 00:50:16 +0900

    Add カスタムログインページを追加

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.demo.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin(login -> login
+                .loginProcessingUrl("/login")
+                .loginPage("/login")
+                .defaultSuccessUrl("/inventory/search")
+                .failureUrl("/login?error")
+                .permitAll()
+        ).logout(logout -> logout
+                .logoutSuccessUrl("/login")
+        ).authorizeHttpRequests(authz -> authz
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                .permitAll()
+                .mvcMatchers("/signup")
+                .permitAll()
+                .anyRequest().authenticated()
+        );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
